3d880ec3c47337578f2586ebeb693fdbc4f7a2fb
Faryne <faryne@gmail.com>
2014-05-18 15:12:43 +0800

    add new files

---
+++ userscripts/userscript.js
@@ -0,0 +1,17 @@
+<div id="overlay" style="position:fixed;width:100%;height:100%;top:0;left:0;background-color: #000;z-index: 10000;opacity: 0.6;">
+	<div id="tips" style="
+    /* text-align: center; */
+    /* margin: 0 auto; */
+    /* width: 800px; */
+    position: absolute;
+    width: 600px;
+    top: 50%;
+    left: 25%;
+    background-color: #FFF;
+    text-align: center;
+    color: #000;
+    padding: 15px 0;
+">
+      Hello World
+  	</div>
+</div>
